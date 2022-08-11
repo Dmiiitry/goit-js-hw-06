53c81d6833124d6752d5a6017c4d684d7a77048a
Dmiiitry <karosplayd@gmail.com>
2022-08-11 14:31:40 +0300

    tasks 1-9

---
+++ src/js/task-04.js
@@ -0,0 +1,40 @@
+const divEl = document.querySelector('#counter');
+
+const btnDecrement = document.querySelector('[data-action = "decrement"]');
+
+const value = document.querySelector('#value');
+
+const btnIncrement = document.querySelector('[data-action = "increment"]');
+
+let counterValue = 0;
+
+btnIncrement.addEventListener('click', handleIncrementButtonClick);
+
+function handleIncrementButtonClick(event) {
+    value.textContent = counterValue += 1;
+}
+
+btnDecrement.addEventListener('click', handleDecrementButtonClick);
+
+function handleDecrementButtonClick(event){
+    value.textContent = counterValue -= 1;
+}
+
+// const divEl = document.querySelector('#counter');
+// const btnDecrement = document.querySelector('[data-action = "decrement"]');
+// const valueEl = document.querySelector('#value');
+// const btnIncrement = document.querySelector('[data-action = "increment"]');
+
+// let counterValue = 0;
+
+// btnIncrement.addEventListener('click', handleIncrementButtonClick);
+
+// function handleIncrementButtonClick(event) {
+//    valueEl.textContent = counterValue += 1;
+// }
+
+// btnDecrement.addEventListener('click', handleDecrementButtonClick);
+
+// function handleDecrementButtonClick(event) {
+//     valueEl.textContent = counterValue -= 1;
+// }
---
+++ src/js/task-06.js
@@ -0,0 +1,21 @@
+let inputStyle = document.querySelector('#validation-input');
+
+
+
+let inputLength = Number(inputStyle.dataset.length);
+
+
+
+const inputEl = document.querySelector('input');
+inputStyle.addEventListener('change', e => {
+  const text = e.target.value;
+
+  if (text.length === inputLength) {
+    inputStyle.classList.add('valid');
+    inputStyle.classList.remove('invalid');
+    
+  } else {
+    inputStyle.classList.remove('valid');
+    inputStyle.classList.add('invalid');
+  }
+});
